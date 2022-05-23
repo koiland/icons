7b2e804ddc1182866fadd83390752e623adede61
Alex Eliseev <oielisieiev@shopmonkey.io>
2022-05-23 02:24:50 +0200

    feat(icons): Add SVGO plugin. FEAT-2

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
 import { babel } from '@rollup/plugin-babel';
 import filesize from 'rollup-plugin-filesize';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/icons/index.js',
+  input: 'src/icons/index.ts',
   output: {
     file: 'dist/index.esm.js',
     format: 'esm',
+    sourcemap: true,
   },
   external: [/@babel\/runtime/, 'react'],
   plugins: [
+    typescript(),
     babel({
       babelHelpers: 'runtime',
       plugins: ['@babel/plugin-transform-runtime'],
