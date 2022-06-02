8f1c4efa0f8e4e098d048b400e6549b1a72036db
Alexander Eliseev <alexander.elsv@gmail.com>
2022-06-02 18:02:58 +0200

    Fix/issue 33/fix workflow (#33)
    * fix(fixes): workflow names. ISSUE-33
    
    * fix(fixes): added releaserc. ISSUE-33
    
    * fix(fixes): added rollup cjs configuration. ISSUE-33
    
    * fix(fixes): fixed package paths. ISSUE-33
    
    Co-authored-by: Alex Eliseev <oielisieiev@shopmonkey.io>

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,26 @@
 import { babel } from '@rollup/plugin-babel';
 import filesize from 'rollup-plugin-filesize';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-export default {
+export default ['esm', 'cjs'].map((format = '') => ({
   input: 'src/icons/index.ts',
   output: {
-    file: 'dist/index.esm.js',
-    format: 'esm',
+    ...(format === 'esm' && { file: 'dist/index.esm.js' }),
+    ...(format === 'cjs' && { file: 'dist/index.js' }),
+    format,
     sourcemap: true,
   },
-  external: [/@babel\/runtime/, 'react'],
   plugins: [
+    peerDepsExternal(),
+    resolve(),
     typescript(),
     babel({
       babelHelpers: 'runtime',
       plugins: ['@babel/plugin-transform-runtime'],
     }),
     filesize(),
-  ],
-};
+  ].concat(format === 'cjs' ? commonjs() : []),
+}));
